f721110b2a72e7546ae2394c27a580fc036bbce5
Edward Kabue <ekabue0@gmail.com>
2022-02-28 18:34:49 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
-const hamburger = document.querySelector(".bars");
-const navLinks = document.querySelector(".nav-items");
-const lines = document.querySelectorAll(".bars span");
-const links = document.querySelectorAll(".link");
+const hamburger = document.querySelector('.bars');
+const navLinks = document.querySelector('.nav-items');
+const lines = document.querySelectorAll('.bars span');
+const links = document.querySelectorAll('.link');
 
-hamburger.addEventListener("click", () => {
-  navLinks.classList.toggle("open");
-  hamburger.classList.toggle("toggle");
+hamburger.addEventListener('click', () => {
+  navLinks.classList.toggle('open');
+  hamburger.classList.toggle('toggle');
   lines.forEach((line) => {
-    line.classList.toggle("color");
+    line.classList.toggle('color');
   });
 });
 links.forEach((l) => {
-  l.addEventListener("click", () => {
-    navLinks.classList.replace("open", "bars");
+  l.addEventListener('click', () => {
+    navLinks.classList.replace('open', 'bars');
   });
 });
