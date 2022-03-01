03c30b4bad804670de2cabcbe413d1b30b66d2ee
Edward Kabue <ekabue0@gmail.com>
2022-03-01 18:37:06 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const hamburger = document.querySelector('.bars');
 const navLinks = document.querySelector('.nav-items');
 const links = document.querySelectorAll('.nav-items li');
+const openModalButtons = document.querySelectorAll('[data-modal-target]');
+const closeModalButtons = document.querySelectorAll('[data-close-button]');
+const overlay = document.getElementById('overlay');
 
 hamburger.addEventListener('click', () => {
   navLinks.classList.toggle('open');
@@ -11,5 +14,38 @@ links.forEach((l) => {
   l.addEventListener('click', () => {
     hamburger.classList.remove('toggle');
     navLinks.classList.remove('open');
+  });
+});
+
+function openModal(modal) {
+  if (modal == null) return;
+  modal.classList.add('active');
+  overlay.classList.add('active');
+}
+
+function closeModal(modal) {
+  if (modal == null) return;
+  modal.classList.remove('active');
+  overlay.classList.remove('active');
+}
+
+openModalButtons.forEach((button) => {
+  button.addEventListener('click', () => {
+    const modal = document.querySelector(button.dataset.modalTarget);
+    openModal(modal);
+  });
+});
+
+overlay.addEventListener('click', () => {
+  const modals = document.querySelectorAll('.modal.active');
+  modals.forEach((modal) => {
+    closeModal(modal);
+  });
+});
+
+closeModalButtons.forEach((button) => {
+  button.addEventListener('click', () => {
+    const modal = button.closest('.modal');
+    closeModal(modal);
   });
 });
