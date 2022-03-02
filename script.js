4e134ce671284d1b4d1a85a20b048b3f95c20877
Edward Kabue <ekabue0@gmail.com>
2022-03-02 16:17:41 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,74 +1,74 @@
-const hamburger = document.querySelector(".bars");
-const navLinks = document.querySelector(".nav-items");
-const links = document.querySelectorAll(".nav-items li");
+const hamburger = document.querySelector('.bars');
+const navLinks = document.querySelector('.nav-items');
+const links = document.querySelectorAll('.nav-items li');
 
-hamburger.addEventListener("click", () => {
-  navLinks.classList.toggle("open");
-  hamburger.classList.toggle("toggle");
+hamburger.addEventListener('click', () => {
+  navLinks.classList.toggle('open');
+  hamburger.classList.toggle('toggle');
 });
 
 links.forEach((l) => {
-  l.addEventListener("click", () => {
-    hamburger.classList.remove("toggle");
-    navLinks.classList.remove("open");
+  l.addEventListener('click', () => {
+    hamburger.classList.remove('toggle');
+    navLinks.classList.remove('open');
   });
 });
 
 /* eslint-disable no-undef */
-const projectsContainer = document.querySelector(".projects-container");
-const popupProject = document.querySelector(".popup-project");
-const overlayDiv = document.querySelector(".overlay");
+const projectsContainer = document.querySelector('.projects-container');
+const popupProject = document.querySelector('.popup-project');
+const overlayDiv = document.querySelector('.overlay');
 
 const projects = [
   {
     id: 1,
-    title: " Tonic",
-    sub: "CANOPY",
-    picture: "./images/SnapshootPortfolio-desk.svg",
-    modalPicture: "./images/modal.png",
+    title: ' Tonic',
+    sub: 'CANOPY',
+    picture: './images/SnapshootPortfolio-desk.svg',
+    modalPicture: './images/modal.png',
     description:
-      "A daily selection of privately personalized reads; no accounts or sign-ups required.",
-    technologies: ["html", "css", "javascript"],
-    types: ["Back End Dev", "2015"],
-    liveLink: "https://google.com",
-    codeLink: "https://github.com",
+      'A daily selection of privately personalized reads; no accounts or sign-ups required.',
+    technologies: ['html', 'css', 'javascript'],
+    types: ['Back End Dev', '2015'],
+    liveLink: 'https://google.com',
+    codeLink: 'https://github.com',
   },
   {
     id: 2,
-    title: "Multi-Post Stories",
-    sub: "FACEBOOK",
-    picture: "./images/second.svg",
-    modalPicture: "./images/modal.png",
+    title: 'Multi-Post Stories',
+    sub: 'FACEBOOK',
+    picture: './images/second.svg',
+    modalPicture: './images/modal.png',
     description:
-      "Experimental content creation feature that allows users to add to an existing story over the course of a day without spamming their friends.",
-    types: ["Back End Dev", "2015"],
-    technologies: ["html", "css", "javascript"],
-    liveLink: "#",
-    codeLink: "#",
+      'Experimental content creation feature that allows users to add to an existing story over the course of a day without spamming their friends.',
+    types: ['Back End Dev', '2015'],
+    technologies: ['html', 'css', 'javascript'],
+    liveLink: '#',
+    codeLink: '#',
   },
   {
     id: 3,
-    title: "Facebook 360",
-    sub: "FACEBOOK",
-    picture: "./images/Snapshoot-Portfolio.svg",
-    modalPicture: "./images/modal.png",
+    title: 'Facebook 360',
+    sub: 'FACEBOOK',
+    picture: './images/Snapshoot-Portfolio.svg',
+    modalPicture: './images/modal.png',
     description:
-      "A single page application that helps managing books by saving them in the LocalStorage.",
-    technologies: ["html", "css", "javascript"],
-    liveLink: "#",
-    codeLink: "#",
+      'A single page application that helps managing books by saving them in the LocalStorage.',
+    technologies: ['html', 'css', 'javascript'],
+    liveLink: '#',
+    codeLink: '#',
   },
   {
     id: 4,
-    title: "Uber Navigation",
-    sub: "Uber",
-    picture: "./images/second.svg",
-    modalPicture: "./images/modal.png",
+    title: 'Uber Navigation',
+    sub: 'Uber',
+    picture: './images/second.svg',
+    modalPicture: './images/modal.png',
     description:
-      "A smart assistant to make driving more safe, efficient, and fun by unlocking your most expensive computer: your car.",
-    technologies: ["html", "css", "javascript"],
-    liveLink: "#",
-    codeLink: "#",
+      'A smart assistant to make driving more safe, efficient, and fun by unlocking your most expensive computer: your car.',
+    technologies: ['html', 'css', 'javascript'],
+    liveLink: '#',
+    codeLink: '#',
   },
 ];
 
@@ -76,19 +76,19 @@ const projects = [
 
 function renderTechnologies(techArr) {
   let technologiesHtml = "<ul class='sub'>";
-  techArr.forEach((tech, index) => {
+  techArr.forEach((tech) => {
     technologiesHtml += `<li class='technology'>${tech}</li>`;
   });
-  technologiesHtml += "</ul>";
+  technologiesHtml += '</ul>';
   return technologiesHtml;
 }
 
 function renderTechnologiesForModal(techArr) {
   let technologiesHtml = "<ul class='sub'>";
-  techArr.forEach((tech, index) => {
+  techArr.forEach((tech => {
     technologiesHtml += `<li class='technology'>${tech}</li>`;
   });
-  technologiesHtml += "</ul>";
+  technologiesHtml += '</ul>';
   return technologiesHtml;
 }
 
@@ -113,8 +113,8 @@ function renderSingleProject(p) {
          </section>
          <section class="prompt">
         <button class='project-details-btn' type='button' data-project-id='${
-          p.id
-        }'>See Project</button>
+  p.id
+}'>See Project</button>
         </section>
     </div>
 </div>
@@ -125,41 +125,38 @@ function renderSingleProject(p) {
 // Render projects dynamically in the HTML document
 projectsContainer.innerHTML = projects
   .map((p) => renderSingleProject(p))
-  .join("");
+  .join('');
 
 // Click event listener to projects buttons;
-const projectDetailsBtns = document.querySelectorAll(".project-details-btn");
+const projectDetailsBtns = document.querySelectorAll('.project-details-btn');
 
 projectDetailsBtns.forEach((btn) => {
-  btn.addEventListener("click", (event) => {
-    const projectId = Number(event.target.getAttribute("data-project-id"));
+  btn.addEventListener('click', (event) => {
+    const projectId = Number(event.target.getAttribute('data-project-id'));
     const projectToShow = projects.find((p) => p.id === projectId);
 
-    popupProject.querySelector(".popup-title").textContent =
-      projectToShow.title;
-    popupProject.querySelector(".sub").textContent = projectToShow.sub;
-    popupProject.querySelector(".popup-image").src = projectToShow.modalPicture;
+    popupProject.querySelector('.popup-title').textContent = projectToShow.title;
+    popupProject.querySelector('.sub').textContent = projectToShow.sub;
+    popupProject.querySelector('.popup-image').src = projectToShow.modalPicture;
     popupProject.querySelectorAll(
-      ".popup-image"
+      '.popup-image',
     ).alt = `Image showing a preview of the project named ${projectToShow.title}`;
-    popupProject.querySelector(".article-e").textContent =
-      projectToShow.description;
-    popupProject.querySelector(".built-with-e").innerHTML =
-      renderTechnologiesForModal(projectToShow.technologies);
-    popupProject.querySelector(".btnw").href = projectToShow.liveLink;
-    popupProject.querySelector(".source").href = projectToShow.codeLink;
+    popupProject.querySelector('.article-e').textContent = projectToShow.description;
+    popupProject.querySelector('.built-with-e').innerHTML = renderTechnologiesForModal(projectToShow.technologies);
+    popupProject.querySelector('.btnw').href = projectToShow.liveLink;
+    popupProject.querySelector('.source').href = projectToShow.codeLink;
 
-    overlayDiv.classList.add("active");
-    popupProject.style.visibility = "visible";
-    popupProject.style.display = "block";
+    overlayDiv.classList.add('active');
+    popupProject.style.visibility = 'visible';
+    popupProject.style.display = 'block';
     popupProject.style.opacity = 1;
 
-    
-    const popupClose = document.querySelector(".popup-close");
-    popupClose.addEventListener("click", () => {
+    // Popup close
+    const popupClose = document.querySelector('.popup-close');
+    popupClose.addEventListener('click', () => {
       popupProject.style.opacity = 0;
-      popupProject.style.visibility = "hidden";
-      overlayDiv.style.display = "none";
+      popupProject.style.visibility = 'hidden';
+      overlayDiv.style.display = 'none';
     });
   });
 });
